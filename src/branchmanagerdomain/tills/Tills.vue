5371f39aab6abaf26bec2271d22bf822d62eeba9
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-31 15:47:31 +0300

    adds transaction id to admin agent

--- src/branchmanagerdomain/tills/Tills.vue
+++ src/branchmanagerdomain/tills/Tills.vue
@@ -400,7 +400,7 @@ onMounted(() => {
                 @click="open(till)"
               ></i> -->
               <span
-                class="p-1 mx-1 rounded-md text-white bg-blue-600 hover:bg-blue-800"
+                class="p-1 mx-1 rounded-md text-white bg-blue-600 hover:bg-blue-200 hover:text-blue-700"
               >
                 <i class="fa-solid fa-pen" @click="edit(till)"></i>
                 Edit
