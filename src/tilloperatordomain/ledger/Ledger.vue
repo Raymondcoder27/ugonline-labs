0d9bed30b74e457a38eaa1164947af0af709c7d3
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-03 15:27:34 +0300

    adds transaction id to admin agent

--- src/tilloperatordomain/ledger/Ledger.vue
+++ src/tilloperatordomain/ledger/Ledger.vue
@@ -318,22 +318,22 @@ onMounted(() => {
                 </div>
               </td>
 
-              <!-- <td class="text-left text-gray-800">
-                <span>{{ transaction.balance.toLocaleString() }}</span>
-              </td> -->
-
               <td class="text-left text-gray-800">
+                <span>{{ transaction.balance.toLocaleString() }}</span>
+              </td>
+
+              <!-- <td class="text-left text-gray-800"> -->
                 <!-- only show the updated balance if the transaction status is approved -->
-                <div
+                <!-- <div
                   v-if="
                     transaction.status === 'pending' ||
                     transaction.status === 'failed'
                   "
                 >
                   <span>--{{ transaction.balance.toLocaleString() }}--</span>
-                </div>
+                </div> -->
                 <!-- <span>{{ transaction.balance.toLocaleString() }}</span> -->
-              </td>
+              <!-- </td> -->
               <td class="text-center">
                 <span class="text-xs">{{
                   convertDateTime(transaction.createdAt)
