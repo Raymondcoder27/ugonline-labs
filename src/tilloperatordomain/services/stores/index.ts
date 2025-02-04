aa3657ffc9f74078c0dd6692003a45bb1309ba0b
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-04 13:01:48 +0300

    adds transaction id

--- src/tilloperatordomain/services/stores/index.ts
+++ src/tilloperatordomain/services/stores/index.ts
@@ -218,34 +218,6 @@ export const useServicesStore = defineStore("services", () => {
 
   const service: Ref<Service | undefined> = ref()
 
-  const createService = async (payload: any) => {
-    return api.post<ServiceResponseInterface>("/registry/v1/create", payload)
-      .then((response: any) => {
-        createServiceResponse.value = response.data
-      })
-  }
-
-  const createServiceSpec = async (payload: any) => {
-    return api.post<ServiceResponseInterface>("/registry/v1/specs/create", payload)
-      .then((response: any) => {
-        createSpecificationResponse.value = response.data
-      })
-  }
-
-  const updateServiceSpec = async (payload: any) => {
-    return api.put<ServiceResponseInterface>("/registry/v1/specs/update", payload)
-      .then((response: any) => {
-        createSpecificationResponse.value = response.data
-      })
-  }
-
-  const editService = async (id: string, payload: any) => {
-    return api.put<ServiceResponseInterface>("/registry/v1/update/" + id, payload)
-      .then((response: any) => {
-        updateServiceResponse.value = response.data
-      })
-  }
-
   // const fetchServices = async (page: number, limit: number) => {
   //   return api.get("/registry/v1?page=" + page + "&limit=" + limit).then((response: any) => {
   //     services.value = response.data.data
@@ -258,54 +230,25 @@ export const useServicesStore = defineStore("services", () => {
     // })
   }
 
-  const fetchServicesByProvider = async (id: string, page: number) => {
-    return api.get("/registry/v1/provider/" + id + "?limit=15&page=" + page).then((response: any) => {
-      services.value = response.data.data
-    })
-  }
-
-  const findServiceSpecById = async (id: any) => {
-    return api.get("/registry/v1/specs/" + id + "/list").then((response: any) => {
-      serviceSpecifications.value = response.data.data
-    })
-  }
-
-  const findService = async (id: any) => {
-    return api.get("/registry/v1/" + id).then((response: any) => {
-      service.value = response.data.data
-    })
-  }
-
-  const findServiceSpec = async (id: string) => {
-    return api.get("/registry/v1/specs/" + id).then((response: any) => {
-      serviceSpecification.value = response.data.data
-    })
-  }
-
-  const updateServiceSpecificationStatus = async (payload: any) => {
-    return api.put<ServiceResponseInterface>("/registry/v1/specs/update/status", payload)
-      .then((response: any) => {
-        statusUpdateResponse.value = response.data
-      })
-  }
+  
 
   return {
     services,
-    service,
-    createServiceResponse,
-    updateServiceResponse,
-    createSpecificationResponse,
-    serviceSpecification,
-    serviceSpecifications,
-    createService,
-    createServiceSpec,
-    updateServiceSpec,
-    editService,
+    // service,
+    // createServiceResponse,
+    // updateServiceResponse,
+    // createSpecificationResponse,
+    // serviceSpecification,
+    // serviceSpecifications,
+    // createService,
+    // createServiceSpec,
+    // updateServiceSpec,
+    // editService,
     fetchServices,
-    findService,
-    fetchServicesByProvider,
-    findServiceSpec,
-    findServiceSpecById,
-    updateServiceSpecificationStatus
+    // findService,
+    // fetchServicesByProvider,
+    // findServiceSpec,
+    // findServiceSpecById,
+    // updateServiceSpecificationStatus
   };
 })
