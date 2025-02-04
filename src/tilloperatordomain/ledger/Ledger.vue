10f61d5fd77249cda2e8eed5126f6c5dbe3aa95e
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-04 11:39:28 +0300

    adds transaction id to admin agent

--- src/tilloperatordomain/ledger/Ledger.vue
+++ src/tilloperatordomain/ledger/Ledger.vue
@@ -338,8 +338,8 @@ onMounted(() => {
                   <!-- <td> -->
                   <label>
                     <span
-                      class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-red-600 bg-red-100 border border-red-200 hover:text-red-700 hover:bg-red-200"
-                      >Failed</span
+                      class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-green-600 bg-green-100 border border-green-200 hover:text-green-700 hover:bg-green-200"
+                      >Edited</span
                     >
                   </label>
                   <!-- </td> -->
