f5a5dc4dff9bc6936a5dc618468c8a5e3edf7402
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-04 10:41:59 +0300

    adds transaction id to admin agent

--- src/tilloperatordomain/ledger/stores/index.ts
+++ src/tilloperatordomain/ledger/stores/index.ts
@@ -255,48 +255,18 @@ export const useBilling = defineStore("billing", () => {
   //   console.log("Adjust Float Ledger response:", data);
   // }
 
-  // async function adjustFloatLedger(payload: RequestFloat) {
-  //   const { data } = await api.post("/till-operator2-float-ledgers", {
-  //     amount: payload.amount,
-  //     // createdAt: new Date().toISOString(),
-  //     description: payload.description,
-  //     status: "pending",
-  //     till: payload.till,
-  //   });
-  //   floatLedgers.value?.push(data.data);
-  //   console.log("Adjust Float Ledger response:", data);
-  // }
-
   async function adjustFloatLedger(payload: RequestFloat) {
-    try {
-
-      const { data } = await api.post("/till-operator2-float-ledgers", {
-        amount: payload.amount,
-        // createdAt: new Date().toISOString(),
-        description: payload.description,
-        status: "pending",
-        till: payload.till,
-      });
-      floatLedgers.value?.push(data.data);
-      console.log("Adjust Float Ledger response:", data);
-
-
-      //approve the record's status in the float ledger too
-      api.put("/till-operator2-float-requests/" + requestId, {
-        floatLedgerRecordId: data.data.id,
-        // status: "approved",
-        // amount: floatRequest.amount,
-        // till: floatRequest.till,
-      });
-
-      // Update local state after successful API call
-      // floatRequest.status = "approved";
-
-      console.log("Float request approved successfully:", data);
-    } catch (error) {
-      console.error("Error approving float request:", error);
-    }
+    const { data } = await api.post("/till-operator2-float-ledgers", {
+      amount: payload.amount,
+      // createdAt: new Date().toISOString(),
+      description: payload.description,
+      status: "pending",
+      till: payload.till,
+    });
+    floatLedgers.value?.push(data.data);
+    console.log("Adjust Float Ledger response:", data);
   }
+
 
 
   return {
