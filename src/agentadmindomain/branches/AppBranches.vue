65e6e9e2e3060f898e9a68a3fa2061c8432307a7
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-12 10:25:49 +0300

    adds transaction id

--- src/agentadmindomain/branches/AppBranches.vue
+++ src/agentadmindomain/branches/AppBranches.vue
@@ -429,7 +429,7 @@ onMounted(() => {
               <span
                 class="rounded-md p-1 mx-1 text-white bg-red-700 hover:bg-red-200 hover:text-red-700"
                 @click="deleteBranch(branch)"
-              ></span>
+              >
                 <!-- <i
                   class="fa-solid fa-store-slash"
                   @click="deleteBranch(branch)"
